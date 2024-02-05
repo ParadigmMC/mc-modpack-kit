f7794a02f42ce36e2ebbdff73dc843aff56f8641
jh-devv <122896463+jh-devv@users.noreply.github.com>
2024-02-05 16:47:32 +0200

    refactor: add nix and linters

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A template designed to streamline the release process of Minecraft modpacks.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+
+      imports = [
+        ./pre-commit-hooks.nix
+      ];
+
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.alejandra
+            pkgs.git
+            pkgs.packwiz
+            pkgs.yamllint
+          ];
+          name = "mc-modpack-kit";
+          DIRENV_LOG_FORMAT = "";
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+          '';
+        };
+
+        formatter = pkgs.alejandra;
+      };
+    };
+}
